a403bd3a5e2d9bbd73fa87c3c82908f2996d7f03
Alexandru Olaru <alxolr@gmail.com>
2017-04-22 20:35:16 +0300

    am adaugat calculeaza salariu

--- function.js
+++ function.js
@@ -3,9 +3,20 @@ function sum(first, second) {
 }
 
 //  console.log(sum(7, 3));
-function multiply(unu,doi) {
+function multiply(unu, doi) {
   return unu * doi;
 }
 
 // console.log(multiply(5, 'ion'))
 
+function calculeazaSalariu(salariu) {
+  let impozit = 0.08 * salariu;
+  let asigurare = 0.09 * salariu;
+  let impozitTotal = impozit + asigurare;
+  let salariuNet = salariu - impozitTotal;
+
+  return salariuNet;
+}
+
+let deCalculat = calculeazaSalariu(5000);
+console.log(deCalculat);
