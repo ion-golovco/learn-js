e69d0d48049b01dc18bbdc0ad371b90451c4c1b4
Alexandru Olaru <alxolr@gmail.com>
2017-04-22 20:22:05 +0300

    Primele mele functii

---
+++ function.js
@@ -0,0 +1,11 @@
+function sum(first, second) {
+  return first + second;
+}
+
+//  console.log(sum(7, 3));
+function multiply(unu,doi) {
+  return unu * doi;
+}
+
+// console.log(multiply(5, 'ion'))
+
